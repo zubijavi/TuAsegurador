2e1549a4a2cd7db8074ca3b06fdb6e26ef9462e1
Javier Zubillaga <zubijavi@gmail.com>
2025-07-07 18:23:54 -0300

    se agrego home, ico y title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import underConst from '../assets/underConst.jpg'
 
 export default function Home() {
   return (
     <>
       <Header />
       <main>
-        <h2>Conocé nuestros seguros</h2>
+        {/* <h2>Pagina En Construccion</h2> */}
+        <img src={underConst} alt="" />
         {/* Más contenido acá */}
       </main>
       <Footer />

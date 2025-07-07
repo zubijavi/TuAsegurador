56fecd9faed32c9a35c8ed5feb0a061a63063ef8
Javier Zubillaga <zubijavi@gmail.com>
2025-07-07 13:00:41 -0300

    Segundo commit tu asegurador

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>© 2025 Javier Zubillaga</p>
+    </footer>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h2>Conocé nuestros seguros</h2>
+        {/* Más contenido acá */}
+      </main>
+      <Footer />
+    </>
+  );
+}

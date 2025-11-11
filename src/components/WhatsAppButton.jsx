c76642bdba5a4b8230e05ce58b145b9e92783788
Javier Zubillaga <zubijavi@gmail.com>
2025-11-11 17:04:33 -0300

    arreglos varios

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -4,7 +4,7 @@ import "./WhatsAppButton.css";
 const WhatsAppButton = () => {
   return (
     <a
-      href="https://wa.me/5493410000000"
+      href="https://wa.me/5493413354935"
       className="whatsapp-float"
       target="_blank"
       rel="noopener noreferrer"

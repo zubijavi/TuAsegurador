f7093c2b1f2f5d3e2e47644ded60a11818b83ab4
Javier Zubillaga <zubijavi@gmail.com>
2026-07-01 14:00:25 -0300

    se cambio el fondo del nav para ver como se ve

--- src/pages/SeguroAutomotor.jsx
+++ src/pages/SeguroAutomotor.jsx
@@ -1,33 +1,75 @@
 import Cotizador from "../components/Cotizador";
+import auto from "../assets/auto.jpg";
 
 export default function SeguroAutomotor() {
-  return (
-    <main className="max-w-6xl mx-auto px-6 py-16 grid md:grid-cols-2 gap-10">
+    return (
+        <>
+            {/* HERO */}
 
-      {/* INFO */}
-      <div>
-        <h1 className="text-3xl font-bold mb-4">
-          Seguro Automotor
-        </h1>
+            <section
 
-        <p className="text-gray-600">
-          Coberturas adaptadas a tu vehículo.
-        </p>
-      </div>
+                className="relative h-[20vh] bg-cover bg-center flex items-center"
 
-      {/* FORMULARIO */}
-      <div className="bg-white shadow rounded-xl p-6">
-        <h2 className="text-xl font-bold mb-4">
-          Cotizá tu seguro
-        </h2>
+                style={{
+                    backgroundImage: `url(${auto})`
+                }}
 
-        <Cotizador type="auto" />
+            >
 
-        <button className="w-full mt-6 bg-blue-700 text-white py-3 rounded-lg">
-          Enviar solicitud
-        </button>
-      </div>
 
-    </main>
-  );
+                <div className="absolute inset-0 bg-black/60" />
+
+
+
+                <div className="relative z-10 px-6 lg:px-32 text-white">
+
+
+                    <h1 className="text-4xl font-bold mb-4">
+
+                        Seguro Automotor
+
+                    </h1>
+
+
+                    <p className="max-w-xl text-lg text-slate-200">
+
+                        Amplia gama de coberturas con las mejores aseguradoras.
+
+                    </p>
+
+
+                </div>
+
+
+
+            </section>
+            <main className="max-w-6xl mx-auto px-6 py-16 grid md:grid-cols-2 gap-10">
+
+                {/* INFO */}
+                <div>
+                    <h1 className="text-3xl font-bold mb-4">
+                        Seguro Automotor
+                    </h1>
+
+                    <p className="text-gray-600">
+                        Coberturas adaptadas a tu vehículo.
+                    </p>
+                </div>
+
+                {/* FORMULARIO */}
+                <div className="bg-white shadow rounded-xl p-6">
+                    <h2 className="text-xl font-bold mb-4">
+                        Cotizá tu seguro
+                    </h2>
+
+                    <Cotizador type="auto" />
+
+                    <button className="w-full mt-6 bg-blue-700 text-white py-3 rounded-lg">
+                        Enviar solicitud
+                    </button>
+                </div>
+
+            </main>
+        </>
+    );
 }
